d3f596239c1a104034889b3396460a16626a35ae
sumit87@dgp <sumit87.dgp@gmail.com>
2022-05-18 09:46:09 +0530

    probably the way to print in form of arrays

--- Trees/roottoleaf.py
+++ Trees/roottoleaf.py
@@ -1,12 +1,8 @@
+from collections import deque
+import collections
 from btree import TreeNode
 
-def root_to_leaf(node):
-    if node is None:
-        return
 
-    root_to_leaf(node.left)
-    root_to_leaf(node.right)
-    print(node.data)
     
         
 
@@ -18,7 +14,30 @@ def main():
     root.left.right = TreeNode(9)
     root.left.left.right = TreeNode(11)
 
-    root_to_leaf(root)
+    storageArray = [None] * 256
+    pathLen = 0
+    
+    def root_to_leaf(node,storageRef,pathLen):
+        
+        if node is None:
+            return
+
+        storageRef[pathLen] = node
+        pathLen += 1
+
+        if node.right is None and node.left is None:
+            printArrayElements(storageRef,pathLen)
+        else:    
+            root_to_leaf(node.left,storageRef,pathLen)
+            root_to_leaf(node.right,storageRef,pathLen)
+        
+
+    root_to_leaf(root,storageArray,pathLen)
+
+def printArrayElements(arraystorage,pathLen):
+    for i in range(pathLen) :
+        print(arraystorage[i].data)
+    print("\n")    
 
 if __name__=="__main__":
     main()
