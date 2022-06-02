2593fc0fedc63978d2d5290312fda67ecbf20a72
sumit87@dgp <sumit87.dgp@gmail.com>
2022-06-02 09:52:32 +0530

    Creating seperate branch for Binary-Search-Trees

---
+++ BST/bstnode.py
@@ -0,0 +1,8 @@
+class BinarySearchTreeNode():
+    def __init__(self,val) -> None:
+        self.left = self.right = None
+        self.data = val
+
+
+if __name__ == "__main__":
+    print("Executing of no use")
